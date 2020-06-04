254d8198ee1be47dac8a15624c2ceb5ca09d7634
carlosgomes1 <48812516+carlosgomes1@users.noreply.github.com>
2020-06-04 20:57:36 -0300

    Backend ok

--- backend/knexfile.ts
+++ backend/knexfile.ts
@@ -3,10 +3,18 @@ import path from "path";
 module.exports = {
     client: "sqlite3",
     connection: {
-        filename: path.resolve(__dirname, "src", "database", "database.sqlite"),
+        filename: path.resolve(
+            __dirname,
+            "src",
+            "database",
+            "database1.sqlite"
+        ),
     },
     migrations: {
         directory: path.resolve(__dirname, "src", "database", "migrations"),
+    },
+    seeds: {
+        directory: path.resolve(__dirname, "src", "database", "seeds"),
     },
     useNullAsDefault: true,
 };
